36ea2d49b6bacc53b94becb9389f0f281d2b1609
Valechek1 <79101581094@yandex.ru>
2021-08-09 17:57:44 +0300

    adding dependencies

---
+++ SlapStepApp/src/screens/Splash.js
@@ -0,0 +1,21 @@
+// In App.js in a new project
+
+import * as React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+const WrapperView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default () => {
+  return (
+    <WrapperView>
+      <Text>Splash Screen</Text>
+    </WrapperView>
+  );
+};
+
+// export default Splash;
