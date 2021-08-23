ed427dca5575bc82326050e91e591822da365202
Valechek1 <79101581094@yandex.ru>
2021-08-23 08:27:22 +0300

    fix sql

--- SlapStepServer/SlapStep.sql
+++ SlapStepServer/SlapStep.sql
@@ -6,16 +6,20 @@ drop table Token;
 CREATE Table Users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name_user VARCHAR(50) NOT NULL,
-  age INT NOT NULL
+  age INT NOT NULL,
+  phone VARCHAR(20) NOT NULL
 );
 CREATE Table Authorization (
   id INT PRIMARY KEY AUTO_INCREMENT,
   login_user VARCHAR(50) NOT NULL,
-  password_user VARCHAR(250)
+  password_user VARCHAR(250),
+  phone VARCHAR(20) NOT NULL,
+  code INT
 );
 CREATE Table Token (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  token VARCHAR(255) NOT NULL
+  token VARCHAR(255) NOT NULL,
+  id_user INT
 );
 SELECT
   *
